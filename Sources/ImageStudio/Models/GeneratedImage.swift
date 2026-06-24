fc333d6d4022847c67dd885331afccd7491aa69d
Zyn <bobbytatum999@gmail.com>
2026-06-23 23:41:35 -0500

    Move GeneratedImage.swift to new ImageStudio structure

---
+++ Sources/ImageStudio/Models/GeneratedImage.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct GeneratedImage: Identifiable, Codable, Equatable, Hashable, Sendable {
+    var id: UUID
+    var prompt: String
+    var revisedPrompt: String?
+    var model: String
+    var quality: String?
+    var width: Int
+    var height: Int
+    var createdAt: Date
+    var remoteURL: URL?
+    var localFileName: String
+
+    init(
+        id: UUID = UUID(),
+        prompt: String,
+        revisedPrompt: String?,
+        model: String,
+        quality: String?,
+        width: Int,
+        height: Int,
+        createdAt: Date = Date(),
+        remoteURL: URL?,
+        localFileName: String
+    ) {
+        self.id = id
+        self.prompt = prompt
+        self.revisedPrompt = revisedPrompt
+        self.model = model
+        self.quality = quality
+        self.width = width
+        self.height = height
+        self.createdAt = createdAt
+        self.remoteURL = remoteURL
+        self.localFileName = localFileName
+    }
+}
