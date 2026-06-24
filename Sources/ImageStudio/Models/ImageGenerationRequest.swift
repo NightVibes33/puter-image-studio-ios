f4f2698209f33ec8f74fa043e81cafb4796e84f9
Zyn <bobbytatum999@gmail.com>
2026-06-24 09:05:17 -0500

    Restore (v5): Sources/ImageStudio/Models/ImageGenerationRequest.swift

--- Sources/ImageStudio/Models/ImageGenerationRequest.swift
+++ Sources/ImageStudio/Models/ImageGenerationRequest.swift
@@ -1,1 +1,40 @@
-Failed to get file contents. The path does not point to a file or directory, or the file does not exist in the repository.
+import Foundation
+
+struct ImageGenerationRequest: Codable, Equatable, Sendable {
+    var prompt: String
+    var model: String
+    var quality: String?
+    var width: Int
+    var height: Int
+    var responseFormat: ResponseFormat
+
+    enum ResponseFormat: String, Codable, Sendable {
+        case url
+        case b64JSON = "b64_json"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case prompt
+        case model
+        case quality
+        case width
+        case height
+        case responseFormat = "response_format"
+    }
+
+    init(
+        prompt: String,
+        model: String,
+        quality: String?,
+        width: Int,
+        height: Int,
+        responseFormat: ResponseFormat = .url
+    ) {
+        self.prompt = prompt
+        self.model = model
+        self.quality = quality
+        self.width = width
+        self.height = height
+        self.responseFormat = responseFormat
+    }
+}
